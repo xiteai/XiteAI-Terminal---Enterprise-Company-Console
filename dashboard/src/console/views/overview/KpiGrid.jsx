1a5ac6f4d38d63578ea3ea55c41ecdc9ab419ee6
SURAJ <incxiteai@gmail.com>
2026-09-26 13:27:27 +0530

    Show downloads beside the other headline numbers

--- dashboard/src/console/views/overview/KpiGrid.jsx
+++ dashboard/src/console/views/overview/KpiGrid.jsx
@@ -12,6 +12,8 @@ export default function Figures({ data, range }) {
       <StatTile label={`Active ${w.many}`} value={num(k.active_period)}
         delta={delta(k.active_period, k.active_prev_period)} period={`prior ${range} days`} />
       <StatTile label="Active today" value={num(k.active_24h)} note={`of ${num(k.total_installs)} ${w.many}`} />
+      <StatTile label="Downloads" value={num(k.downloads_period)}
+        note={`${num(k.downloads_total)} all time`} />
       <StatTile label="On latest" value={pct(k.on_latest)} note={`version ${data.latest_version || "–"}`} />
       {product.kind === "web"
         ? <StatTile label={`New ${w.many}`} value={num(k.new_installs)}
